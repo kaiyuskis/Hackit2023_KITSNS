670e2db138227640573e736e71a77b3be197e08d
kaiyuskis <yuki_skier@icloud.com>
2023-08-13 21:54:14 +0900

    a

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 setup(
-    name='flaskr',
-    version='1.0.0',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'flask',
+    name='YourPackageName',       # パッケージ名
+    version='0.1',                 # バージョン番号
+    packages=find_packages(),      # パッケージのサブディレクトリを自動で検出
+    install_requires=[             # 依存パッケージのリスト
+        'Flask==2.0.1',
+        'Flask-SQLAlchemy==2.5.1',
+        'Flask-Login==0.5.0',
+        'email-validator==1.1.3',
     ],
+    author='Your Name',            # 作者名
+    author_email='your@email.com', # 作者のメールアドレス
+    description='Your package description',  # パッケージの説明
+    url='https://github.com/yourusername/yourpackage',  # パッケージのリポジトリURL
 )
